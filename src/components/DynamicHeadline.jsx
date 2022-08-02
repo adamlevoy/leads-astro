8d35192a8ae4008ef588a80f2d70bc9aa6e36c0c
Difool <difool0x@gmail.com>
2022-08-02 12:51:52 -0700

    added dynamic keyword insertion; minor updates

---
+++ src/components/DynamicHeadline.jsx
@@ -0,0 +1,33 @@
+import React, { useState, useEffect } from 'react';
+
+const DynamicHeadline = ({ prefix = 'free', suffix = 'matchmaking' }) => {
+	const param = 'lpkw';
+	const [keyword, setKeyword] = useState('Executive Coach');
+
+	function keywordInsert() {
+		if (window.location.search) {
+			const queryString = window.location.search;
+			console.log(queryString);
+			const urlParams = new URLSearchParams(queryString);
+			const searchQuery = urlParams.get(param);
+			setKeyword(searchQuery);
+			console.log(searchQuery);
+		} else {
+			console.log(keyword);
+		}
+	}
+
+	useEffect(() => {
+		keywordInsert();
+	}, []);
+
+	return (
+		<h1 className='fs-primary-heading fw-bold text-neutral-100 uppercase'>
+			{prefix}
+			<br></br> <span className='text-primary-400'>{keyword}</span>
+			<br></br> {suffix}
+		</h1>
+	);
+};
+
+export default DynamicHeadline;
