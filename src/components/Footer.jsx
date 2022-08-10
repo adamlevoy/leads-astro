9591d0430298b1729fa5c5f291f86e235e90044e
Difool <difool0x@gmail.com>
2022-08-10 11:42:43 -0700

    added persistant url params to footer; minor copy + style updates

---
+++ src/components/Footer.jsx
@@ -0,0 +1,25 @@
+import React, { useState, useEffect } from 'react';
+import Logo from './Logo';
+import { getUrlParams } from '../utils/getUrlParams';
+
+const Footer = () => {
+	const [urlParams, setUrlParams] = useState('');
+
+	useEffect(() => {
+		setUrlParams(getUrlParams);
+	}, []);
+
+	return (
+		<footer class='p-block-700'>
+			<Logo />
+			<nav>
+				<a href={`/contact-us${urlParams}`}>Contact Us</a>
+				<a href={`/terms-of-service${urlParams}`}>Terms of Service</a>
+				<a href={`/privacy-policy${urlParams}`}>Privacy Policy</a>
+				<p className='text-neutral-100'>&copy; Copyright 2022</p>
+			</nav>
+		</footer>
+	);
+};
+
+export default Footer;
